7a3745a89b38f5fa55813457f6f4cff0d598b624
matthewjhough <matthough1@icloud.com>
2018-06-17 18:03:37 -0400

    added basic QR Scanning

--- src/containers/Scanner.js
+++ src/containers/Scanner.js
@@ -1,64 +1,139 @@
 import React, { Component } from "react";
 import {
-  AppRegistry,
-  StyleSheet,
+  Alert,
+  Linking,
+  Dimensions,
+  LayoutAnimation,
   Text,
   View,
-  TouchableOpacity,
-  Linking
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity
 } from "react-native";
+import { BarCodeScanner, Permissions } from "expo";
 
-// import QRCodeScanner from "react-native-qrcode-scanner";
+export default class Scanner extends Component {
+  state = {
+    hasCameraPermission: null,
+    lastScannedUrl: null
+  };
 
-class Scanner extends Component {
-  onSuccess(e) {
-    Linking.openURL(e.data).catch(err =>
-      console.error("An error occured", err)
-    );
+  componentDidMount() {
+    this._requestCameraPermission();
   }
+
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === "granted"
+    });
+  };
+
+  _handleBarCodeRead = result => {
+    if (result.data !== this.state.lastScannedUrl) {
+      LayoutAnimation.spring();
+      this.setState({ lastScannedUrl: result.data });
+    }
+  };
 
   render() {
     return (
-      <View>
-        <Text>Scanner Class reload</Text>
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ? (
+          <Text>Requesting for camera permission</Text>
+        ) : this.state.hasCameraPermission === false ? (
+          <Text style={{ color: "#fff" }}>
+            Camera permission is not granted
+          </Text>
+        ) : (
+          <BarCodeScanner
+            onBarCodeRead={this._handleBarCodeRead}
+            style={{
+              height: Dimensions.get("window").height,
+              width: Dimensions.get("window").width
+            }}
+          />
+        )}
+
+        {this._maybeRenderUrl()}
+
+        <StatusBar hidden />
       </View>
-      //   <QRCodeScanner
-      //     onRead={this.onSuccess.bind(this)}
-      //     topContent={
-      //       <Text style={styles.centerText}>
-      //         Go to{" "}
-      //         <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-      //         your computer and scan the QR code.
-      //       </Text>
-      //     }
-      //     bottomContent={
-      //       <TouchableOpacity style={styles.buttonTouchable}>
-      //         <Text style={styles.buttonText}>OK. Got it!</Text>
-      //       </TouchableOpacity>
-      //     }
-      //   />
     );
   }
+
+  _handlePressUrl = () => {
+    Alert.alert(
+      "Open this URL?",
+      this.state.lastScannedUrl,
+      [
+        {
+          text: "Yes",
+          onPress: () => Linking.openURL(this.state.lastScannedUrl)
+        },
+        { text: "No", onPress: () => {} }
+      ],
+      { cancellable: false }
+    );
+  };
+
+  _handlePressCancel = () => {
+    this.setState({ lastScannedUrl: null });
+  };
+
+  _maybeRenderUrl = () => {
+    if (!this.state.lastScannedUrl) {
+      return;
+    }
+
+    return (
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
+          <Text numberOfLines={1} style={styles.urlText}>
+            {this.state.lastScannedUrl}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.cancelButton}
+          onPress={this._handlePressCancel}
+        >
+          <Text style={styles.cancelButtonText}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 }
 
 const styles = StyleSheet.create({
-  centerText: {
+  container: {
     flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: "#777"
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000"
   },
-  textBold: {
-    fontWeight: "500",
-    color: "#000"
+  bottomBar: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    padding: 15,
+    flexDirection: "row"
   },
-  buttonText: {
-    fontSize: 21,
-    color: "rgb(0,122,255)"
+  url: {
+    flex: 1
   },
-  buttonTouchable: {
-    padding: 16
+  urlText: {
+    color: "#fff",
+    fontSize: 20
+  },
+  cancelButton: {
+    marginLeft: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  cancelButtonText: {
+    color: "rgba(255,255,255,0.8)",
+    fontSize: 18
   }
 });
-
-export default Scanner;
