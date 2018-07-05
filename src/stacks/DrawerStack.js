a8a168f958b06a0ae743b178b7b1d7b4710c58e2
matthewjhough <matthough1@icloud.com>
2018-07-04 20:35:28 -0400

    added location tracking to app

--- src/stacks/DrawerStack.js
+++ src/stacks/DrawerStack.js
@@ -2,12 +2,14 @@ import { DrawerNavigator, createDrawerNavigator } from "react-navigation";
 import Root from "../Root";
 import Rates from "../containers/Rates";
 import Scanner from "../containers/Scanner";
+import LocationTracker from "../containers/Location";
 
 // main burger menu navigation, authenticated screens
 const DrawerStack = createDrawerNavigator({
   Home: { screen: Root },
   Rates: { screen: Rates },
-  Scanner: { screen: Scanner }
+  Scanner: { screen: Scanner },
+  Location: { screen: LocationTracker }
 });
 
 export default DrawerStack;
